14d31a882c11f1b1b0343621cea5ffe590acb940
Yan Britos <yanbritos@gmail.com>
2025-07-14 11:41:19 -0300

    fix: usar porta do ambiente

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ app.post("/api/foto", async (req, res) => {
   }
 });
 
-app.listen(3001, () => {
-  console.log("✅ API rodando em http://localhost:3001");
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`✅ API rodando na porta ${PORT}`);
 });
